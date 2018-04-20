cdedb449168a9d9b848509990149b7c832e5beed
Thuy Pham <tpham045@hotmail.com>
2018-04-20 10:33:50 -0700

    setup: switch to react-router v4 `react-router-dom`
    - install `react-router-dom`
    - to-do: doc
    - react-router-dom guide: https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf

---
+++ shinyspork/src/NoteContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// noteList json
+import noteList from './noteList.js';
+
+class NoteContainer extends Component {
+  render() {
+      console.log(noteList)
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default NoteContainer;
---
+++ shinyspork/src/noteList.js
@@ -0,0 +1,5 @@
+export const noteList = {[
+    {id: 0, title: "first note", content: "Hello World!"},
+    {id: 1, title: "second note", content: "I am second note"},
+    {id: 2, title: "last note", content: "Goodbye!"}
+]}
