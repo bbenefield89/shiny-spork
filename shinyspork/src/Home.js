6ed5963f597b9a38494b98677a0467bcf14c0c02
Thuy Pham <tpham045@hotmail.com>
2018-04-19 17:28:00 -0700

    First commit

---
+++ shinyspork/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <body>This is Home page</body>
+      </div>
+    );
+  }
+}
+
+export default Home;
