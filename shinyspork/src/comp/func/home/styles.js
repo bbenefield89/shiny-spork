36a4a749b3facfb0605b1ab03628da3b195daa92
Brandon Benefield <bsquared18@gmail.com>
2018-04-22 14:14:02 -0400

    move Home component from "class" to "func" directory

---
+++ shinyspork/src/comp/func/home/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  nav_header: {
+    background: {
+      color: '#eee',
+    },
+
+    // '& $nav_header__nav': {
+    //   background: {
+    //     color: 'red',
+    //   },
+    // },
+  },
+
+  nav_header__nav: {},
+}
+
+export default styles;
