c44860fb7fcc0772fda74b4d4c9004d163b3bf39
Brandon Benefield <bsquared18@gmail.com>
2018-04-22 13:32:31 -0400

    Reformat Home.js navLink var is now an array of objects consistin of "name" and "url" keys A little formatting on the navigation element for readability

--- shinyspork/src/comp/class/home/Home.js
+++ shinyspork/src/comp/class/home/Home.js
@@ -2,9 +2,55 @@ import React, { Component } from 'react';
 import injectSheet from 'react-jss';
 import styles from './styles';
 
+const showButtons = true;
+const navLinks = [
+  { 
+    name: 'Demo',
+    url : '/demo',
+  },{
+    name: 'Log in',
+    url : '#'
+  },{
+    name: 'Sign up',
+    url : '#'
+  },{
+    name: 'Log out',
+    url : '#'
+  }
+];
+
 const Home = ({classes}) => (
   <div className={classes.root}>
-    <button className={classes.button}>Click Me</button>
+    <header className={classes.nav_header}>
+      <h1>Shiny Spork</h1>
+
+      <nav className={ classes.nav_header__nav }>
+        {
+          /*
+          ** conditional
+          ** if `showButtons` === true
+          */
+          (showButtons) ? (
+            <ul className={ classes.nav_header__nav__list }>
+              {
+                // map through `navLinks`
+                navLinks.map(link => {
+                  return (
+                    <li className={ classes.nav_header__nav__list__item }>
+                      <a href={ link.url }>{ link.name }</a>
+                    </li>
+                )})
+              }
+            </ul>
+            ) : (
+            // else only render `Demo`
+            <ul>
+              <li><a href="/demo">Demo</a></li>
+            </ul>
+            )
+        }
+      </nav>
+    </header>
   </div>
 )
 
