09b06311b05061f727ceb2a3ce57ba8509721ff0
Brandon Benefield <bsquared18@gmail.com>
2018-04-23 19:42:22 -0400

    add style to new elements

--- shinyspork/src/comp/func/home/styles.js
+++ shinyspork/src/comp/func/home/styles.js
@@ -1,17 +1,32 @@
 const styles = {
   nav_header: {
     background: {
-      color: '#eee',
+      'color': '#eee',
     },
-
-    // '& $nav_header__nav': {
-    //   background: {
-    //     color: 'red',
-    //   },
-    // },
+    'display': 'flex',
   },
 
-  nav_header__nav: {},
-}
+  nav_header__nav: {
+    'display': 'flex',
+    'flex-grow': '1',
+  },
+
+  nav_header__nav__list: {
+    'display': 'flex',
+    'flex-grow': '1',
+    'align-items': 'center',
+    'justify-content': 'space-around',
+    'list-style': 'none',
+  },
+
+  nav_header__nav__list__item__link: {
+    'background': '#6cca60',
+    'border-radius': '5px',
+    'color': '#111',
+    'font-size': '1.5rem',
+    'padding': '20px',
+    'text-decoration': 'none',
+  }
+};
 
 export default styles;
