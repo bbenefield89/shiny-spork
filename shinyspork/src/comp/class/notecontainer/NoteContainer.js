35f6d2e308f4706109f47b3c8a46c088baf5252a
Thuy Pham <tpham045@hotmail.com>
2018-05-05 11:11:09 -0700

    setup: merge the latest #17

--- shinyspork/src/comp/class/notecontainer/NoteContainer.js
+++ shinyspork/src/comp/class/notecontainer/NoteContainer.js
@@ -13,21 +13,10 @@ const styles = {
 
 class NoteContainer extends Component {
   render() {
-<<<<<<< HEAD
     const { notes, handleSelect, handleDelete, classes } = this.props;
     return (
       <div className={classes.root}>
       {notes.map(note => (
-        <Note key={note._id} title={note.note_title} handleSelect={handleSelect.bind(this, note._id)} handleDelete={handleDelete.bind(this, note._id)}/>
-      ))}
-      </div>
-    )
-  }
-=======
-    const { notes, handleSelect, handleDelete } = this.props;
-
-    return (
-      notes.map(note => (
         <Note
           key={note._id}
           id={ note._id }
@@ -36,10 +25,10 @@ class NoteContainer extends Component {
           // handleDelete={handleDelete.bind(this, note._id)}
           handleDelete={ handleDelete }
         />
-      ))
-    );
-  };
->>>>>>> eda2966546918d478c8f32f1357deb2a8a60147a
+      ))}
+      </div>
+    )
+  }
 }
 
 NoteContainer.propTypes = {
